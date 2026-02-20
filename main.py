746c158febdcb4d80a9cdd4914d0b1bbbdc8d6d0
ibabdulaziz2197 <ibabdulaziz2197@gmail.com>
2026-02-20 16:32:20 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+# 11
+def takror(a):
+    print(a)
+
+takror("Abdulaziz")
+
+# 12
+def takror(a,):
+    print(a)
+
+takror(123)
+
+# 13
+def qoshish(a,b):
+    print(12+10)
+
+qoshish()
+
+# 14
+def takror(a, b):
+    print(f"salom {a}, yosh: {b}")
+
+# 15
+def kvadrat(a):
+    print(a*a)
+kvadrat(3)
+
+# 16
+def matn(a, b):
+    print(a * b)
+
+# 17
+def r(a, b):
+    print(a -b)
+
+# 18
+def ism(a):
+    print(f"Hurmatli: {a}")
+
+# 19
+
+def kop(a, b):
+    print(a * b)
+kop(3, 8)
